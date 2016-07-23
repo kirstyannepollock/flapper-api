c30d7252ae520801da0c4d57e6b777961c3d426c
kirstyannepollock <kpollock@hotmail.co.uk>
2016-07-23 18:35:20 +0200

    really this time. with comment on request.query

--- app.js
+++ app.js
@@ -4,10 +4,13 @@ var express = require("express");
 var app = express();
 
 app.use(function(request, response, next){
+  //console.log("the leaves are ", request.query.colour);
   next();
 });
 
 var port = process.env.PORT || 3000;
+
+
 
 app.listen(port, function(){
   console.log("express server is listening on port " + port);
