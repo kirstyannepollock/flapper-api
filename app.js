275d2c42ca0668ad48a9f0e888e4c774555047e8
kirstyannepollock <kpollock@hotmail.co.uk>
2016-07-23 18:34:38 +0200

    request.query used

--- app.js
+++ app.js
@@ -3,6 +3,10 @@
 var express = require("express");
 var app = express();
 
+app.use(function(request, response, next){
+  next();
+});
+
 var port = process.env.PORT || 3000;
 
 app.listen(port, function(){
