172340872efe03a20b5bf43f8cac269eef022b49
kirstyannepollock <kpollock@hotmail.co.uk>
2016-07-24 13:03:32 +0200

    Tidied and commented

--- mongoose_sandbox.js
+++ mongoose_sandbox.js
@@ -8,7 +8,7 @@ db.on("error", function(err){
   console.log("connection error!", err)
 });
 
-
+//=========== general mongoose save code ==========
 function logClose(){
   console.log("db connnection closed");
 }
@@ -26,31 +26,32 @@ function checkSaveError(error){
   };
 }
 
-
-//{items: []}
-function arraySave(error){  
-    checkSaveError(error);
-    
-    // Have to do this to treat like an array
-    var itemArray = this.items;
-
-    if(itemArray.length > 1 ){
-      var currItem = itemArray.shift();
-      var params = {items: itemArray};
-      currItem.save(arraySave.bind(params));
-
-    }
-    else{
-      // last item
-      var currItem = itemArray.shift();
-      currItem.save(checkErrorAndCloseDb);
-    };    
-}
-
 function checkErrorAndCloseDb(error){  
     checkSaveError(error);    
     closeDb();
 }
+
+// Recursively saves all items in the array passed in as "items".
+// Bind params: {items: []} (optionally a first:true param 
+// to stop the error check the first time round).
+function arraySave(error){
+  // really shouldn't do this the first time...  
+    if(!this.first){
+      checkSaveError(error); 
+    }
+
+    if(this.items.length > 1 ){
+      var currItem = this.items.shift();
+      var params = {items: this.items};
+      currItem.save(arraySave.bind(params));
+    }
+    else{
+      // last item
+      var currItem = this.items.shift();
+      currItem.save(checkErrorAndCloseDb);
+    };    
+}
+//=======================================================
 
 function createAnimalSchema(){
   var Schema = mongoose.Schema;
@@ -81,7 +82,7 @@ function mainCode(){
   });
 
   var animal = new Animal ({}); //goldfish
-  var params = {items: [animal, elephant]};
+  var params = {items: [animal, elephant], first: true};
   
   // remove all existing and then save all changes
   Animal.remove({}, arraySave.bind(params));
