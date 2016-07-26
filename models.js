08639d4252144f46eba05d9188900fed50461a20
kirstyannepollock <kpollock@hotmail.co.uk>
2016-07-26 10:44:39 +0200

    Questions and Answers relation and hooks sorted to do the NoSQL business and assign defaults on save, update

--- models.js
+++ models.js
@@ -4,6 +4,19 @@ var mongoose = require("mongoose");
 
 var Schema = mongoose.Schema;
 
+function sortAnswers(a,b)
+{
+  // -ve a before b
+  // 0 equal
+  // +ve a after b
+  if(a.votes === b.votes)
+  {
+    return b.updatedAt - a.updatedAt; // this should work...
+  };
+
+  return b.votes - a.votes;
+}
+
 // Simple 2 related tables with max 20 Answers per Question,
 // Therefore embed Answers as array in Question.
 var AnswerSchema = new Schema(
@@ -15,6 +28,25 @@ var AnswerSchema = new Schema(
   }
 );
 
+// instance method
+AnswersSchema.methods.update = function(updates,callback)
+{
+  // merge answers into Question document
+  // this === Answers
+  Object.assign(this, updates, {updatedAt: new Date() })
+
+  //save the changes (need to save the parent)
+  this.parent().save(callback);
+}
+
+AnswersSchema.methods("vote", function (vote, callback)
+{
+  this.votes = vote =="up" ? this.votes + 1 : this.votes -1;
+
+  //save the changes (need to save the parent)
+  this.parent().save(callback);
+}
+
 var QuestionSchema = new Schema(
   {
     text: string,
@@ -23,6 +55,22 @@ var QuestionSchema = new Schema(
   }
 );
 
+//Sort the answers before we save
+//Guess this is life in the NoSQL world...
+//(though we could hook to the "find" method),
+//
+// I guess if we KNOW we (almost) always want 
+// the data in a specific order  - but ALL apps
+// that use the db must conform to this std.
+// Guess we only offer access via an API
+// and never the db directly. Hmmm... 
+QuestionSchema.pre("Save", function(next)
+{
+  this.answers.sort(sortAnswers);
+  next();
+});
 
 var Question = mongoose.model("Question", QuestionSchema);
+
+
 module.exports.Question = Question;
