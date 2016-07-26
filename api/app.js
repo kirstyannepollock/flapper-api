3919caa7e52642d5fcbf0bc0a7f151d6b540ace3
kirstyannepollock <kpollock@hotmail.co.uk>
2016-07-26 13:29:39 +0200

    API all working. Front End example app added.

---
+++ api/app.js
@@ -0,0 +1,68 @@
+'use strict';
+
+//================= libraries ==================
+var express = require("express");
+var jsonParser = require("body-parser").json;
+var logger = require("morgan");
+//===============================================
+
+//=============== our app =======================
+var app = express();
+var routes = require("./routes");
+//===============================================
+
+//================= db functions ================
+function dbCode()
+{
+  console.log("connected to db");
+}
+//===============================================
+
+//Logger
+app.use(logger("dev"));
+
+//JSON parsing
+app.use(jsonParser());
+
+//Mongoose
+var mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost:27017/sandbox");
+var db = mongoose.connection;
+
+db.on("error", function(err)
+{
+  console.log("connection error!", err)
+});
+
+//all db code
+db.once("open", dbCode); 
+
+//CORS
+app.use(function (request, result, next)
+{
+  response.header("Access-Control-Allow-Origin","*");
+  response.header("Access-Control-Allow-Headers","Origin, X-Requested_With, Content-Type, Accept");
+});
+
+app.use("/questions",routes);
+
+// catch 404 and forward
+app.use(function(request, response, next){
+  var err = new Error("Not Found");
+  err.status = 404;
+  next(err);
+});
+
+//error handler
+app.use(function(err,request, response, next){
+  response.status(err.status || 500);
+  response.json({ error: { message: err.message } })
+});
+
+var port = process.env.PORT || 3000;
+
+app.listen(port, function(){
+  console.log("express server is listening on port " + port);
+});
+
+
